ba453b276c07dcee4f357eaa8c5a5cda6c4f9f0c
Kamil Mrówka <kamil31245@gmail.com>
2025-10-31 15:58:23 +0100

    Cleanup => Move all geospacial functions to geo_csv

--- programs/geo_csv.py
+++ programs/geo_csv.py
@@ -1,10 +1,11 @@
 from dataclasses import dataclass, asdict
-from typing import List, Optional, Dict
+from typing import List, Optional, Dict, cast
 import csv
 import os
 from shapely import wkt
 from shapely.geometry import MultiLineString
 import copy
+from shapely.wkt import loads
 
 
 @dataclass
@@ -140,3 +141,69 @@ def collapse_street(streets: List[GeoNode]):
     collapsed_street.geom = multi_line_string.wkt
 
     return collapsed_street
+
+def merge_streets(streets: List[GeoNode]) -> List[GeoNode]:
+    merged:List[GeoNode] = []
+    by_city = merge_streets_by_city(streets)
+
+    for s in by_city:
+        city_streets = by_city[s]
+        merged_city_streets = merge_streets_by_distance(city_streets)
+        for merged_street in merged_city_streets:
+            merged.append(merged_street)
+
+    return merged
+
+
+def merge_streets_by_city(streets: List[GeoNode]):
+    streets_sorted: Dict[str, List[GeoNode]] = {}
+
+    for street in streets:
+        street_id = f"{street.name_primary}-{street.city_name}"
+
+        if street_id not in streets_sorted:
+            streets_sorted[street_id] = [street]
+        else:
+            streets_sorted[street_id].append(street)
+    return streets_sorted
+
+
+def merge_streets_by_distance(streets: List[GeoNode]) -> List[GeoNode]:
+    unmerged: List[GeoNode] = [u for u in streets if u.geom is not None]
+    merged: List[GeoNode] = []
+
+    while (len(unmerged) > 0):
+        print("WHILE HIGHER")
+        base = unmerged[0]
+
+        if (len(unmerged) == 1):
+            unmerged.remove(base)
+            merged.append(base)
+            break
+
+        currently_merged: GeoNode = base
+        current_geom = loads(cast(str, currently_merged.geom))
+        
+        while True:
+            print("WHILE LOWER")
+            currently_merging: List[GeoNode] = []
+            merged_anything = False
+
+            for u in unmerged[1:]:
+                geom = loads(cast(str, u.geom))
+                if current_geom.distance(geom) < 0.005:
+                    currently_merged = collapse_street([currently_merged, u])
+                    current_geom = loads(cast(str, currently_merged.geom))
+                    currently_merging.append(u)
+                    merged_anything = True
+
+            for s in currently_merging:
+                unmerged.remove(s)
+
+            if (not merged_anything):
+                merged.append(currently_merged)
+                break
+            
+        unmerged.remove(base)
+
+    return merged
--- programs/main.py
+++ programs/main.py
@@ -1,7 +1,5 @@
-from geo_csv import GeoNode, collapse_street, read_geo_csv
 import sys
-from typing import Dict, List, cast
-from shapely.wkt import loads
+from geo_csv import read_geo_csv, merge_streets
 
 
 def main():
@@ -11,78 +9,21 @@ def main():
     cities = nodes["cities"]
     neighbourhoods = nodes["neighbourhoods"]
     districts = nodes["districts"]
+
     collapsed_streets = merge_streets(streets)
 
     for street in streets:
-        print("Street: ", street.name_primary)
+        print("Street: ", f"{street.name_primary}-{street.district_name}-{street.city_name}")
+    for district in districts:
+        print("District: ", f"{district.name_primary}")
+    for city in cities:
+        print("City: ", f"{city.name_primary}")
+    for neighbourhood in neighbourhoods:
+        print("Neighbourhood: ", f"{neighbourhood.name_primary}")
+
+    print("\n\n\n\n")
     for street in collapsed_streets:
-        print("Street Collapsed: ", street.name_primary)
-
-def merge_streets(streets: List[GeoNode]) -> List[GeoNode]:
-    merged:List[GeoNode] = []
-    by_city = merge_streets_by_city(streets)
-
-    for s in by_city:
-        city_streets = by_city[s]
-        merged_city_streets = merge_streets_by_distance(city_streets)
-        for merged_street in merged_city_streets:
-            merged.append(merged_street)
-
-    return merged
-
-
-def merge_streets_by_city(streets: List[GeoNode]):
-    streets_sorted: Dict[str, List[GeoNode]] = {}
-
-    for street in streets:
-        street_id = f"{street.name_primary}-{street.city_name}"
-
-        if street_id not in streets_sorted:
-            streets_sorted[street_id] = [street]
-        else:
-            streets_sorted[street_id].append(street)
-    return streets_sorted
-
-
-def merge_streets_by_distance(streets: List[GeoNode]) -> List[GeoNode]:
-    unmerged: List[GeoNode] = [u for u in streets if u.geom is not None]
-    merged: List[GeoNode] = []
-
-    while (len(unmerged) > 0):
-        print("WHILE HIGHER")
-        base = unmerged[0]
-
-        if (len(unmerged) == 1):
-            unmerged.remove(base)
-            merged.append(base)
-            break
-
-        currently_merged: GeoNode = base
-        current_geom = loads(cast(str, currently_merged.geom))
-        
-        while True:
-            print("WHILE LOWER")
-            currently_merging: List[GeoNode] = []
-            merged_anything = False
-
-            for u in unmerged[1:]:
-                geom = loads(cast(str, u.geom))
-                if current_geom.distance(geom) < 0.005:
-                    currently_merged = collapse_street([currently_merged, u])
-                    current_geom = loads(cast(str, currently_merged.geom))
-                    currently_merging.append(u)
-                    merged_anything = True
-
-            for s in currently_merging:
-                unmerged.remove(s)
-
-            if (not merged_anything):
-                merged.append(currently_merged)
-                break
-            
-        unmerged.remove(base)
-
-    return merged
+        print("Street Collapsed: ", f"{street.name_primary}-{street.district_name}-{street.city_name}")
 
 
 def getDataPath():
